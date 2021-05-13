ab6733fb3d7dc68d19369b1b61aa5ea10da81f94
rejohnbd <rejohn.cse@gmail.com>
2021-05-14 04:36:25 +0600

    Change in Form Style

--- src/components/formItems/FormInputGroup.js
+++ src/components/formItems/FormInputGroup.js
@@ -12,19 +12,26 @@ const FormInputGroup = ({
   error,
 }) => {
   return (
-    <div className="form-group">
-      <label htmlFor={id}>{label}</label>
-      <input
-        id={id}
-        name={name}
-        type={type}
-        className={`form-control ${error ? "is-invalid" : ""}`}
-        required={required}
-        placeholder={placeholder}
-        onChange={onChange}
-        value={value}
-      />
-      {error && <small className="form-text text-danger">{error}</small>}
+    <div className="form-group row">
+      <label
+        htmlFor={id}
+        className="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right"
+      >
+        {label} <span className="required-label">*</span>
+      </label>
+      <div className="col-lg-4 col-md-9 col-sm-8">
+        <input
+          id={id}
+          name={name}
+          type={type}
+          className={`form-control ${error ? "is-invalid" : ""}`}
+          required={required}
+          placeholder={placeholder}
+          onChange={onChange}
+          value={value}
+        />
+        {error && <small className="form-text text-danger">{error}</small>}
+      </div>
     </div>
   );
 };
